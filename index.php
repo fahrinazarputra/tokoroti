9a887e4f202693a58cc3c53f92bf235c18012bbd
fahrinazarputra <fahrinazar6@gmail.com>
2025-09-04 13:05:40 +0700

    tokoroti

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Metadata sesuai perintah -->
+     <meta name="keywords" content="Borocake, cake murah, toko roti,">
+     <meta name="description" content="Website Borocake">
+
+    <link rel="stylesheet" href="styles/bootstrap.min.css">
+    <link rel="shorcut icon" href="assets/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="styles/styles.css">
+    <title>BoroCake</title>
+</head>
+<body>
+    <!--  -->
+    
+</body>
+</html>
